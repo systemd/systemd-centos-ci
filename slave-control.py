6a4a58ba1e4a613a813c6d51c235dfecd58c608b
Daniel Mack <daniel@zonque.org>
2016-02-22 16:26:31 +0100

    lots of improvements

--- slave-control.py
+++ slave-control.py
@@ -7,9 +7,10 @@
 # based on a script by Karanbir Singh
 #
 
-import json, urllib, subprocess, sys
+import json, urllib, subprocess, sys, argparse
 
-key = open("duffy.key", "r").read().rstrip()
+github_base = "https://github.com/systemd/"
+git_name = "systemd-centos-ci"
 
 def duffy_cmd(cmd, params):
 	url_base = "http://admin.ci.centos.org:8080"
@@ -17,22 +18,68 @@ def duffy_cmd(cmd, params):
 	print "debug:: url = " + url
 	return urllib.urlopen(url).read()
 
-ver = "7"
-arch = "x86_64"
-count = 1
+def remote_exec(host, cmd, log):
+	ssh_cmd = "ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@%s '%s'" % (host, cmd)
+	print "debug:: cmd = " + ssh_cmd
 
-git_url = "https://github.com/systemd/systemd-centos-ci.git"
+	try:
+		output = subprocess.check_output(ssh_cmd, shell = True)
 
-params = { "key": key, "ver": ver, "count": count, "arch":  arch }
-data = duffy_cmd("/Node/get", params)
+		if log:
+			log.write(output)
+			log.close
 
-b = json.loads(data)
-for h in b['hosts']:
-	cmd = "ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@%s 'echo %s'" % (h, git_url)
-	print "debug:: cmd = " + cmd
-	rtn_code = subprocess.call(cmd, shell=True)
+	except subprocess.CalledProcessError as e:
+		if log:
+			log.write(str(e))
+			log.write(e.output)
+			log.close
 
-params = { "key": key, "ssid": b['ssid'] }
-data = duffy_cmd("/Node/done", params)
+		raise
 
-sys.exit(rtn_code)
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--log',  help = 'Logfile for command output')
+	parser.add_argument('--ver',  help = 'CentOS version', default = '7')
+	parser.add_argument('--arch', help = 'Architecture', default = 'x86_64')
+	parser.add_argument('--host', help = 'Use an already provisioned build host')
+	parser.add_argument('--pr',   help = 'Pull request ID')
+	args = parser.parse_args()
+
+	key = open("duffy.key", "r").read().rstrip()
+
+	if args.log:
+		log = open(args.log, "w")
+	else:
+		log = None
+
+	if args.host:
+		host = args.host
+		ssid = None
+	else:
+		params = { "key": key, "ver": args.ver, "arch":  args.arch }
+		data = duffy_cmd("/Node/get", params)
+
+		json_data = json.loads(data)
+		host = json_data['hosts'][0]
+		ssid = json_data['ssid']
+
+	ret = 0
+
+	try:
+		bootstrap_cmd = "yum install -y git && git clone %s%s.git && %s/slave/bootstrap.sh %s" % (github_base, git_name, git_name, args.pr)
+		remote_exec(host, bootstrap_cmd, log)
+
+	except Exception as e:
+		print "Execution failed! See logfile for details: %s" % str(e)
+		ret = 255
+
+	finally:
+		if ssid:
+			params = { "key": key, "ssid": ssid }
+			duffy_cmd("/Node/done", params)
+
+	sys.exit(ret)
+
+if __name__ == "__main__":
+	main()
--- slave/bootstrap.sh
+++ slave/bootstrap.sh
@@ -5,15 +5,20 @@ set -e
 pr=$1
 
 curl 'http://copr.fedorainfracloud.org/coprs/lnykryn/systemd-centosci-environment/repo/epel-7/lnykryn-systemd-centosci-environment-epel-7.repo' -o /etc/yum.repos.d/lnykryn-systemd-centosci-environment-epel-7.repo
+yum -y update
 yum -y install systemd-ci-environment
+
+test -f systemd && rm -rf systemd
 git clone https://github.com/systemd/systemd.git
 
-exit 0
+git fetch -fu origin refs/pull/$pr/head:pr
+git checkout pr
 
 ./autogen.sh
 ./configure CFLAGS='-g -O0 -ftrapv' --sysconfdir=/etc --localstatedir=/var --libdir=/usr/lib64
 
 make -j10
+make install
 
 # readahead is dead in systemd upstream
 rm -f /usr/lib/systemd/system/systemd-readahead-done.service
