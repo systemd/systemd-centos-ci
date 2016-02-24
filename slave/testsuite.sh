45bd52504e9de0c000e14b44472cd7be7a632ab2
Daniel Mack <daniel@zonque.org>
2016-02-24 20:31:14 +0100

    typo

--- slave/testsuite.sh
+++ slave/testsuite.sh
@@ -6,7 +6,7 @@ yum -qy install net-tools strace
 
 cd systemd
 
-for t in 01-BASIC 03-JOBS 04-JOURNAL 05-RLIMITS do
+for t in 01-BASIC 03-JOBS 04-JOURNAL 05-RLIMITS; do
 	PKG_CONFIG_PATH=../../src/core make -C test/TEST-$t/ clean setup run  INITRD=/initrd.img
 done
 
