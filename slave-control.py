7bccf393209f8a28f613149bce762f4bc984dc6a
Daniel Mack <daniel@zonque.org>
2016-02-25 12:09:17 +0100

    some more commands

--- slave-control.py
+++ slave-control.py
@@ -13,6 +13,18 @@ def duffy_cmd(cmd, params):
 	print "Duffy API debug: url = " + url
 	return urllib.urlopen(url).read()
 
+def host_done(key, ssid):
+	params = { "key": key, "ssid": ssid }
+	duffy_cmd("/Node/done", params)
+	print "Duffy: Host with ssid %s marked as done" % ssid
+
+def log_msg(msg, logfile):
+	print msg
+
+	if logfile:
+		logfile.write(msg + "\n")
+		logfile.write("======================================================\n");
+
 def remote_exec(host, remote_cmd, logfile, expected_ret = 0):
 	cmd = [ '/usr/bin/ssh',
 		'-t',
@@ -22,12 +34,9 @@ def remote_exec(host, remote_cmd, logfile, expected_ret = 0):
 		'-l', 'root',
 		host, remote_cmd ]
 
-	l = ">>> Executing remote command: '%s' on %s" % (remote_cmd, host)
-	print l
+	log_msg(">>> Executing remote command: '%s' on %s" % (remote_cmd, host), logfile)
 
 	if logfile:
-		logfile.write(l + "\n")
-		logfile.write("======================================================\n");
 		logfd = logfile.fileno()
 	else:
 		logfd = None
@@ -40,31 +49,38 @@ def remote_exec(host, remote_cmd, logfile, expected_ret = 0):
 
 	end = time.time()
 
-	l = "<<< Remote command finished after %.1f seconds, return code = %d" % (end - start, p.returncode)
-	print l
-
-	if logfile:
-		logfile.write(l + "\n")
-		logfile.write("======================================================\n");
+	log_msg("<<< Remote command finished after %.1f seconds, return code = %d" % (end - start, p.returncode), logfile)
 
 	if p.returncode != expected_ret:
 		raise Exception("Remote command returned code %d, bailing out." % p.returncode)
+
+def ping_host(host, logfile)
+	cmd = [ '/usr/bin/ping', '-c', '1', host ]
+
+	p = subprocess.Popen(cmd, stdout = logfd, stderr = logfd, shell = False, bufsize = 1)
+	p.communicate()
+	p.wait()
+
+	log_msg("Host %s appears reachable again", logfile)
 
 def reboot_host(host, logfile):
 	# the reboot command races against the graceful exit, so ignore the return code in this case
 	remote_exec(host, "journalctl -b --no-pager && reboot", logfile, 255)
 
-	# wait for the host to reappear
-	time.sleep(90)
+	time.sleep(30)
+	ping_host(host, logfile)
+	time.sleep(20)
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--log',  help = 'Logfile for command output')
-	parser.add_argument('--ver',  help = 'CentOS version', default = '7')
-	parser.add_argument('--arch', help = 'Architecture', default = 'x86_64')
-	parser.add_argument('--host', help = 'Use an already provisioned build host')
-	parser.add_argument('--keep', help = 'Do not kill provisioned build host')
-	parser.add_argument('--pr',   help = 'Pull request ID')
+	parser.add_argument('--log',            help = 'Logfile for command output')
+	parser.add_argument('--ver',            help = 'CentOS version', default = '7')
+	parser.add_argument('--arch',           help = 'Architecture', default = 'x86_64')
+	parser.add_argument('--host',           help = 'Use an already provisioned build host')
+	parser.add_argument('--pr',             help = 'Pull request ID')
+	parser.add_argument('--keep',           help = 'Do not kill provisioned build host', action = 'store_const', const = True)
+	parser.add_argument('--kill-host',      help = 'Mark a provisioned host as done and bail out')
+	parser.add_argument('--kill-all-hosts', help = 'Mark all provisioned hosts as done and bail out', action = 'store_const', const = True)
 	args = parser.parse_args()
 
 	key = open("duffy.key", "r").read().rstrip()
@@ -74,20 +90,36 @@ def main():
 	else:
 		logfile = None
 
+	if args.kill_host:
+		host_done(key, args.kill_host)
+		return 0
+
+	if args.kill_all_hosts:
+		params = { "key": key }
+		json_data = duffy_cmd("/Inventory", params)
+		data = json.loads(json_data)
+
+		for host in data:
+			host_done(key, host[1])
+
+		return 0
+
 	if args.host:
 		host = args.host
 		ssid = None
 	else:
 		params = { "key": key, "ver": args.ver, "arch": args.arch }
-		data = duffy_cmd("/Node/get", params)
+		json_data = duffy_cmd("/Node/get", params)
+		data = json.loads(json_data)
 
-		json_data = json.loads(data)
-		host = json_data['hosts'][0]
-		ssid = json_data['ssid']
+		host = data['hosts'][0]
+		ssid = data['ssid']
 
 		print "Duffy: Host provisioning successful, hostname = %s, ssid = %s" % (host, ssid)
 
 	ret = 0
+
+	start = time.time()
 
 	try:
 		cmd = "yum install -y git && git clone %s%s.git && %s/slave/bootstrap.sh %s" % (github_base, git_name, git_name, args.pr)
@@ -101,7 +133,7 @@ def main():
 #		cmd = "%s/slave/cockpit.sh" % git_name
 #		remote_exec(host, cmd, logfile)
 
-		for i in range(3):
+		for i in range(2):
 			cmd = "exit `journalctl --list-boots | wc -l`"
 			remote_exec(host, cmd, logfile, i + 1)
 
@@ -113,11 +145,10 @@ def main():
 			cmd = "systemctl --failed --all | grep -q '^0 loaded'"
 			remote_exec(host, cmd, logfile)
 
-	except Exception as e:
-		l = "XXX Execution failed! See logfile for details: %s" % str(e)
-		if logfile:
-			logfile.write("ERROR: %s\n" % l)
+		log_msg("All tests succeeded.", logfile)
 
+	except Exception as e:
+		log_msg("Execution failed! See logfile for details: %s" % str(e), logfile)
 		ret = 255
 
 	finally:
@@ -125,11 +156,13 @@ def main():
 			if args.keep:
 				print "Keeping host %s, ssid = %s" % (host, ssid)
 			else:
-				params = { "key": key, "ssid": ssid }
-				duffy_cmd("/Node/done", params)
-				print "Duffy: Host %s marked as done, ssid = %s" % (host, ssid)
+				host_done(key, ssid);
 
-		logfile.close()
+		end = time.time()
+		log_msg("Total time %.1f seconds" % (end - start), logfile)
+
+		if logfile:
+			logfile.close()
 
 	sys.exit(ret)
 
