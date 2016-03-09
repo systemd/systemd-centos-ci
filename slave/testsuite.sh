b3edc20b5e45fc02907c425c9ceed68c3a542a7b
Branislav Blaskovic <bblaskov@redhat.com>
2016-03-09 13:59:57 +0100

    Revert changes to testsuite.sh and bootstrap.sh

--- slave/testsuite.sh
+++ slave/testsuite.sh
@@ -4,21 +4,11 @@ set -e
 
 yum -qy install net-tools strace
 
-pushd systemd
+cd systemd
 
-# Unit tests
 for t in 01-BASIC 03-JOBS 04-JOURNAL 05-RLIMITS 07-ISSUE-1981; do
 	d=test/TEST-$t/
 	test -d $d && \
 		PKG_CONFIG_PATH=../../src/core make -C test/TEST-$t/ clean setup run INITRD=/initrd.img
 done
-popd
 
-# System tests
-pushd tests
-for t in *; do
-	pushd $t
-	./runtest.sh
-	popd
-done
-popd
