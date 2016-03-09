315143a4058d2ce709120f0ad003204b2d4a1f09
Branislav Blaskovic <bblaskov@redhat.com>
2016-03-09 13:58:21 +0100

    system tests in one file

---
+++ slave/system-tests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+# Prepare environment
+git clone https://git.fedorahosted.org/git/beakerlib.git
+make -C beakerlib
+make -C beakerlib install
+
+# Run testsuite
+for t in tests/*; do
+    pushd $t
+    ./runtest.sh
+    popd
+done
+
