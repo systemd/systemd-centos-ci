19ca1029bbb2bc2305f117c929d99c90e69fee2e
Daniel Mack <daniel@zonque.org>
2016-07-08 13:55:47 +0200

    run built-in testsuite as well

--- slave/testsuite.sh
+++ slave/testsuite.sh
@@ -6,10 +6,7 @@ yum -qy install net-tools strace
 
 cd systemd
 
-#adduser test
-#chmod a+x ..
-#chown -R test:test .
-#su test -c "make check"
+make check
 
 for t in 01-BASIC 03-JOBS 04-JOURNAL 05-RLIMITS 07-ISSUE-1981; do
 	d=test/TEST-$t/
