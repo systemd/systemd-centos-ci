2dfd51df365812f2fe2538b185e6e0f7da548eef
Frantisek Sumsal <fsumsal@redhat.com>
2018-10-18 09:55:26 +0200

    Install dependencies before each test

--- slave/system-tests.sh
+++ slave/system-tests.sh
@@ -7,10 +7,29 @@ git clone https://github.com/beakerlib/beakerlib.git
 make -C beakerlib
 make -C beakerlib install
 
+# Append 'rlGetTestState' to each test, so it returns a correct exit code
+while read file; do
+    if ! grep -Pzq "rlGetTestState[[:space:]]*\z" "$file"; then
+        echo -ne "\nrlGetTestState\n" >> "$file"
+    fi
+done <<< "$(find tests/ -type f -name "runtest.sh")"
+
 # Run testsuite
 for t in tests/*; do
     pushd $t
+
+    # Install test dependencies
+    DEPS="$(awk '
+        match($0, /\"Requires:[[:space:]]*(.*)\"/, m) {
+            print m[1];
+        }' Makefile)"
+    if [ ! -z "$DEPS" ]; then
+        yum -y -q install $DEPS
+    fi
+
+    # Execute the test
     ./runtest.sh
+
     popd
 done
 
