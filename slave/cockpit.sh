ac690d72c0c316790e83f8513922e0e2e08beb61
Frantisek Sumsal <fsumsal@redhat.com>
2018-10-18 16:34:56 +0200

    Fix cockpit.sh script
    Use nodejs/npm from the EPEL repo

--- slave/cockpit.sh
+++ slave/cockpit.sh
@@ -2,20 +2,16 @@
 
 set -e
 
-curl -sL https://rpm.nodesource.com/setup | bash -
-yum install -y nodejs json-glib-devel polkit-devel krb5-devel libssh-devel pcp-libs-devel \
+yum -y -q install epel-release yum-utils
+yum-config-manager -q --enable epel
+yum install -y npm json-glib-devel polkit-devel krb5-devel libssh-devel pcp-libs-devel \
         xmlto python-libguestfs qemu mock qemu-kvm rpm-build \
         curl libvirt-client libvirt-python libvirt python-lxml \
         krb5-workstation krb5-server selinux-policy-devel openssl \
         libguestfs-tools expect rsync
 
-npm -g install npm@latest-2
-npm -g install phantomjs
-
 git clone https://github.com/systemd/cockpit.git
-
 cd cockpit
-
 git checkout ci
 
 yum-builddep -y tools/cockpit.spec
