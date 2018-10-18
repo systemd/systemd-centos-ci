3da9283728bbfc7491111505ba71787499c5c7a4
Frantisek Sumsal <fsumsal@redhat.com>
2018-10-18 13:11:48 +0200

    Temporarily skip tests/TEST-24-UNIT-TESTS

--- slave/testsuite.sh
+++ slave/testsuite.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/sh
 
+# Temporarily skipped test
+# - TEST-24 doesn't work in nspawn, investigate
+
+SKIP_LIST=(
+    "test/TEST-24-UNIT-TESTS"
+)
+
 set -e
 
 yum -q -y install net-tools strace nc busybox
@@ -9,6 +16,9 @@ cd systemd
 #make check
 
 for t in test/TEST-*; do
+    if [[ " ${SKIP_LIST[@]} " =~ " $t " ]]; then
+        continue
+    fi
     test -d $t && \
         PKG_CONFIG_PATH=../../src/core make -C $t clean setup run INITRD=/initrd.img
 done
