e703e1bc4146392dc030197cd7f2d7d11ed6680c
Frantisek Sumsal <fsumsal@redhat.com>
2018-10-18 12:41:42 +0200

    Run all TEST-* tests

--- slave/testsuite.sh
+++ slave/testsuite.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-yum -q -y install net-tools strace
+yum -q -y install net-tools strace nc busybox
 
 cd systemd
 
 #make check
 
-for t in 01-BASIC 03-JOBS 04-JOURNAL 05-RLIMITS 07-ISSUE-1981; do
-    d=test/TEST-$t/
-    test -d $d && \
-        PKG_CONFIG_PATH=../../src/core make -C test/TEST-$t/ clean setup run INITRD=/initrd.img
+for t in test/TEST-*; do
+    test -d $t && \
+        PKG_CONFIG_PATH=../../src/core make -C $t clean setup run INITRD=/initrd.img
 done
 
