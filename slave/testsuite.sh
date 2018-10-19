4dda41ecb7f686e1b7e6cc7345a708667bb4a9c0
Frantisek Sumsal <fsumsal@redhat.com>
2018-10-19 10:44:08 +0200

    Make use of the run-integration-tests.sh script

--- slave/testsuite.sh
+++ slave/testsuite.sh
@@ -8,8 +8,15 @@ cd systemd
 
 #make check
 
-for t in test/TEST-*; do
-    test -d $t && \
-        PKG_CONFIG_PATH=../../src/core make -C $t clean setup run INITRD=/initrd.img
-done
+#for t in test/TEST-*; do
+#    test -d $t && \
+#        PKG_CONFIG_PATH=../../src/core make -C $t clean setup run INITRD=/initrd.img
+#done
+
+# Workaround for older ninja-build versions
+if [ ! -f /usr/bin/ninja ]; then
+    ln -s /usr/bin/ninja-build /usr/bin/ninja
+fi
+
+test/run-integration-tests.sh
 
