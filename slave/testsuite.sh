1e0e1930e0b1b68cea90d2a445231d191d0c0838
Frantisek Sumsal <fsumsal@redhat.com>
2018-10-19 11:20:23 +0200

    Run 'make check' (ninja test)

--- slave/testsuite.sh
+++ slave/testsuite.sh
@@ -2,21 +2,20 @@
 
 set -e
 
-yum -q -y install net-tools strace nc busybox
-
-cd systemd
-
-#make check
-
-#for t in test/TEST-*; do
-#    test -d $t && \
-#        PKG_CONFIG_PATH=../../src/core make -C $t clean setup run INITRD=/initrd.img
-#done
-
 # Workaround for older ninja-build versions
 if [ ! -f /usr/bin/ninja ]; then
     ln -s /usr/bin/ninja-build /usr/bin/ninja
 fi
 
+yum -q -y install net-tools strace nc busybox
+
+cd systemd
+
+# Run the internal unit tests (make check)
+ninja -C build test
+
+# Run the internal integration testsuite
 test/run-integration-tests.sh
+
+# Other integration tests
 
