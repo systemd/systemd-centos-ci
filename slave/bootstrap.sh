b4f7dc7d5f85c0e8cd1d5fc3057324fc9dd41663
Frantisek Sumsal <fsumsal@redhat.com>
2018-10-19 22:22:35 +0200

    Use python -m pip instead of pip

--- slave/bootstrap.sh
+++ slave/bootstrap.sh
@@ -10,7 +10,7 @@ yum-config-manager -q --enable epel
 yum -q -y update
 yum -q -y install systemd-ci-environment python-lxml python36 ninja-build
 python3.6 -m ensurepip
-pip3.6 install meson
+python3.6 -m pip install meson
 
 # python36 package doesn't create the python3 symlink
 rm -f /usr/bin/python3
