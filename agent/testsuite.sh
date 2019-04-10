846034112a989fb62f0b9594b07d98051350ff0c
Frantisek Sumsal <fsumsal@redhat.com>
2019-04-10 16:26:05 +0200

    agent: stress test TEST-16-EXTEND-TIMEOUT

--- agent/testsuite.sh
+++ agent/testsuite.sh
@@ -36,44 +36,49 @@ cd systemd
 exectask "ninja-test" "meson test -C build --timeout-multiplier=3"
 
 ## Integration test suite ##
-SKIP_LIST=(
-    "test/TEST-16-EXTEND-TIMEOUT" # flaky test
-)
+SKIP_LIST=()
 
 [ ! -f /usr/bin/qemu-kvm ] && ln -s /usr/libexec/qemu-kvm /usr/bin/qemu-kvm
 qemu-kvm --version
 
-for t in test/TEST-??-*; do
-    if [[ " ${SKIP_LIST[@]} " =~ " $t " ]]; then
-        echo -e "\n[SKIP] Skipping test $t"
-        continue
+for i in {0..100}; do
+    for t in test/TEST-{02,03,13,16}*; do
+        if [[ " ${SKIP_LIST[@]} " =~ " $t " ]]; then
+            echo -e "\n[SKIP] Skipping test $t"
+            continue
+        fi
+
+        ## Configure test environment
+        # Explicitly set paths to initramfs and kernel images (for QEMU tests)
+        export INITRD="/boot/initramfs-$(uname -r).img"
+        export KERNEL_BIN="/boot/vmlinuz-$(uname -r)"
+        # Explicitly enable user namespaces
+        export KERNEL_APPEND="user_namespace.enable=1"
+        # Set timeouts for QEMU and nspawn tests to kill them in case they get stuck
+        export QEMU_TIMEOUT=600
+        export NSPAWN_TIMEOUT=600
+        # Set the test dir to something predictable so we can refer to it later
+        export TESTDIR="/var/tmp/systemd-test-${t##*/}"
+        # Set QEMU_SMP appropriately (regarding the parallelism)
+        # OPTIMAL_QEMU_SMP is part of the common/task-control.sh file
+        export QEMU_SMP=$OPTIMAL_QEMU_SMP
+        # Use a "unique" name for each nspawn container to prevent scope clash
+        export NSPAWN_ARGUMENTS="--machine=${t##*/}"
+        export KERNEL_APPEND="systemd.log_target=console systemd.log_level=debug"
+
+        rm -fr "$TESTDIR"
+        mkdir -p "$TESTDIR"
+
+        exectask_p "${t##*/}" "make -C $t clean setup run clean-again"
+    done
+
+    # Wait for remaining running tasks
+    exectask_p_finish
+
+    if [[ $FAILED -ne 0 ]]; then
+        exit 1
     fi
-
-    ## Configure test environment
-    # Explicitly set paths to initramfs and kernel images (for QEMU tests)
-    export INITRD="/boot/initramfs-$(uname -r).img"
-    export KERNEL_BIN="/boot/vmlinuz-$(uname -r)"
-    # Explicitly enable user namespaces
-    export KERNEL_APPEND="user_namespace.enable=1"
-    # Set timeouts for QEMU and nspawn tests to kill them in case they get stuck
-    export QEMU_TIMEOUT=600
-    export NSPAWN_TIMEOUT=600
-    # Set the test dir to something predictable so we can refer to it later
-    export TESTDIR="/var/tmp/systemd-test-${t##*/}"
-    # Set QEMU_SMP appropriately (regarding the parallelism)
-    # OPTIMAL_QEMU_SMP is part of the common/task-control.sh file
-    export QEMU_SMP=$OPTIMAL_QEMU_SMP
-    # Use a "unique" name for each nspawn container to prevent scope clash
-    export NSPAWN_ARGUMENTS="--machine=${t##*/}"
-
-    rm -fr "$TESTDIR"
-    mkdir -p "$TESTDIR"
-
-    exectask_p "${t##*/}" "make -C $t clean setup run clean-again"
 done
-
-# Wait for remaining running tasks
-exectask_p_finish
 
 # Save journals created by integration tests
 for t in test/TEST-??-*; do
