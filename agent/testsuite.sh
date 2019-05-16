0148a89292aca55180f6d2992ca13a00276d2d53
Frantisek Sumsal <fsumsal@redhat.com>
2019-05-16 08:56:40 +0200

    debug-only: systemd-networkd-tests.py stress test

--- agent/testsuite.sh
+++ agent/testsuite.sh
@@ -35,61 +35,16 @@ cd systemd
 # Run the internal unit tests (make check)
 exectask "ninja-test" "meson test -C build --print-errorlogs --timeout-multiplier=3"
 
-## Integration test suite ##
-SKIP_LIST=(
-    "test/TEST-16-EXTEND-TIMEOUT" # flaky test
-)
-
-[[ ! -f /usr/bin/qemu-kvm ]] && ln -s /usr/libexec/qemu-kvm /usr/bin/qemu-kvm
-qemu-kvm --version
-
-for t in test/TEST-??-*; do
-    if [[ ${#SKIP_LIST[@]} -ne 0 && " ${SKIP_LIST[@]} " =~ " $t " ]]; then
-        echo -e "\n[SKIP] Skipping test $t"
-        continue
-    fi
-
-    ## Configure test environment
-    # Explicitly set paths to initramfs and kernel images (for QEMU tests)
-    export INITRD="/boot/initramfs-$(uname -r).img"
-    export KERNEL_BIN="/boot/vmlinuz-$(uname -r)"
-    # Explicitly enable user namespaces
-    export KERNEL_APPEND="user_namespace.enable=1"
-    # Set timeouts for QEMU and nspawn tests to kill them in case they get stuck
-    export QEMU_TIMEOUT=600
-    export NSPAWN_TIMEOUT=600
-    # Set the test dir to something predictable so we can refer to it later
-    export TESTDIR="/var/tmp/systemd-test-${t##*/}"
-    # Set QEMU_SMP appropriately (regarding the parallelism)
-    # OPTIMAL_QEMU_SMP is part of the common/task-control.sh file
-    export QEMU_SMP=$OPTIMAL_QEMU_SMP
-    # Use a "unique" name for each nspawn container to prevent scope clash
-    export NSPAWN_ARGUMENTS="--machine=${t##*/}"
-
-    rm -fr "$TESTDIR"
-    mkdir -p "$TESTDIR"
-
-    exectask_p "${t##*/}" "make -C $t clean setup run clean-again"
-done
-
-# Wait for remaining running tasks
-exectask_p_finish
-
-# Save journals created by integration tests
-for t in test/TEST-??-*; do
-    if [[ -d /var/tmp/systemd-test-${t##*/}/journal ]]; then
-        rsync -aq "/var/tmp/systemd-test-${t##*/}/journal" "$LOGDIR/${t##*/}"
-    fi
-done
-
 ## Other integration tests ##
 TEST_LIST=(
-    "test/test-exec-deserialization.py"
     "test/test-network/systemd-networkd-tests.py"
 )
 
+set -e
 for t in "${TEST_LIST[@]}"; do
-    exectask "${t##*/}" "timeout 30m ./$t"
+    for ((i = 0; i < 100; i++)); do
+        exectask "${t##*/}_$i" "timeout 30m ./$t"
+    done
 done
 
 # Summary
