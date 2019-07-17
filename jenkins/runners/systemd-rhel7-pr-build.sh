4272982b17f68cf034f2b119bca32d413d63c738
Frantisek Sumsal <fsumsal@redhat.com>
2019-07-17 15:02:21 +0200

    jenkins: add bootstrap scripts for RHEL 7 and RHEL 8

---
+++ jenkins/runners/systemd-rhel7-pr-build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Note: this script MUST be self-contained - i.e. it MUST NOT source any
+# external scripts as it is used as a bootstrap script, thus it's
+# fetched and executed without rest of this repository
+#
+# Example usage in Jenkins
+# #!/bin/sh
+#
+# set -e
+#
+# curl -q -o runner.sh https://../systemd-rhel7-pr-build.sh
+# chmod +x runner.sh
+# ./runner.sh
+
+# Add a local ~/bin dir to path for custom binaries (currently used only
+# for the tree binary for generating the artifact landing page)
+export PATH="/home/systemd/bin:$PATH"
+ARGS=
+
+set -e
+set -o pipefail
+
+if [ "$ghprbPullId" ]; then
+    ARGS="$ARGS --pr $ghprbPullId "
+fi
+
+git clone https://github.com/systemd/systemd-centos-ci
+cd systemd-centos-ci
+
+./agent-control.py --rhel 7 $ARGS
