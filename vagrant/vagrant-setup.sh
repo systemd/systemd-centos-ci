797587b715510a2443cec95af8d8878185914a8a
Frantisek Sumsal <fsumsal@redhat.com>
2019-09-26 15:05:25 +0200

    vagrant: enable nested KVM
    As nested KVM got much more stable in recent years, let's give it a try
    and attempt to use it by default to speed up tests under Vagrant.
    
    The nested KVM setup requires two steps:
    1) Enable nested KVM on the host system (modprobe kvm_* nested=1)
    2) Tell the vagrant-libvirt plugin to use it (domain.nested = true)

--- vagrant/vagrant-setup.sh
+++ vagrant/vagrant-setup.sh
@@ -6,8 +6,32 @@
 
 VAGRANT_PKG_URL="https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.rpm"
 
-set -e -u
 set -o pipefail
+set -e -u
+
+# Set up nested KVM
+# Let's make all errors "soft", at least for now, as we're still perfectly
+# fine with running tests without nested KVM
+if KVM_MODULE_NAME="$(lsmod | grep -m1 -Eo '(kvm_intel|kvm_amd)')"; then
+    echo "[vagrant-setup] Detected KVM module: $KVM_MODULE_NAME"
+    # Attempt to reload the detected KVM module with nested=1 parameter
+    if modprobe -v -r $KVM_MODULE_NAME && modprobe -v $KVM_MODULE_NAME nested=1; then
+        # The reload was successful, check if the module's 'nested' parameter
+        # confirms that nested KVM is indeed enabled
+        KVM_MODULE_NESTED="$(< /sys/module/$KVM_MODULE_NAME/parameters/nested)" || :
+        echo "[vagrant-setup] /sys/module/$KVM_MODULE_NAME/parameters/nested: $KVM_MODULE_NESTED"
+
+        if [[ "$KVM_MODULE_NESTED" =~ (1|Y) ]]; then
+            echo "[vagrant-setup] Nested KVM is enabled"
+        else
+            echo "[vagrant-setup] Failed to enable nested KVM"
+        fi
+    else
+        echo "[vagrant-setup] Failed to reload module '$KVM_MODULE_SETUP'"
+    fi
+else
+    echo "[vagrant-setup] No KVM module found, can't setup nested KVM"
+fi
 
 if ! vagrant version 2>/dev/null; then
     # Install Vagrant
