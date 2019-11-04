e8513c16a16d3ea7ee227af0105379ffbbff8cd7
Frantisek Sumsal <fsumsal@redhat.com>
2019-11-04 13:19:13 +0100

    tree-wide: replace certain CFLAGS with their native meson options
    CFLAGS=-O1 => --optimization=1
    CFLAGS=-g  => --buildtype=debug

--- agent/bootstrap.sh
+++ agent/bootstrap.sh
@@ -91,7 +91,9 @@ systemctl disable firewalld
 (
     # Make sure we copy over the meson logs even if the compilation fails
     trap "[[ -d $PWD/build/meson-logs ]] && cp -r $PWD/build/meson-logs '$LOGDIR'" EXIT
-    meson build -Dc_args='-g -O1 -fno-omit-frame-pointer -ftrapv' \
+    meson build -Dc_args='-fno-omit-frame-pointer -ftrapv' \
+                --buildtype=debug \
+                --optimization=1 \
                 --werror \
                 -Dslow-tests=true \
                 -Dtests=unsafe \
