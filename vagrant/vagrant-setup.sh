17e2b0f08f1dd510dc8ada8c5193a29c5483f78b
Frantisek Sumsal <fsumsal@redhat.com>
2019-11-08 16:06:04 +0100

    vagrant: bump Vagrant to v2.2.6

--- vagrant/vagrant-setup.sh
+++ vagrant/vagrant-setup.sh
@@ -4,7 +4,7 @@
 # vagrant-libvirt module to support "proper" virtualization (kvm/qemu) instead
 # of default containers.
 
-VAGRANT_PKG_URL="https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.rpm"
+VAGRANT_PKG_URL="https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.rpm"
 
 set -o pipefail
 set -e -u
