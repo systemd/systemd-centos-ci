d6317a07a8809721b1cad059aa9a09e78841b3c5
Frantisek Sumsal <fsumsal@redhat.com>
2019-11-21 11:20:49 +0100

    agent: bump the upstream CentOS 7 job to CentOS 8
    This will help with several things, especially:
     - drop the plethora of CentOS 7 vs upstream systemd workarounds, like
       the custom Copr repo with various *mangled* dependencies
     - support systemd-homed (see: systemd/systemd#14096)
     - run more tests which depend on newer kernel's features

--- agent/bootstrap.sh
+++ agent/bootstrap.sh
@@ -22,49 +22,15 @@ trap at_exit EXIT
 set -e -u
 set -o pipefail
 
-COPR_REPO="https://copr.fedorainfracloud.org/coprs/mrc0mmand/systemd-centos-ci/repo/epel-7/mrc0mmand-systemd-centos-ci-epel-7.repo"
-COPR_REPO_PATH="/etc/yum.repos.d/${COPR_REPO##*/}"
+ADDITIONAL_DEPS=(libasan libubsan make net-tools qemu-kvm quota strace)
 
-# Enable necessary repositories and install required packages
-#   - enable custom Copr repo with newer versions of certain packages (necessary
-#     to sucessfully compile upstream systemd on CentOS)
-#   - enable EPEL repo for additional dependencies
-#   - update current system
-#   - install python 3.6 (required by meson) and install meson + other build deps
-curl "$COPR_REPO" -o "$COPR_REPO_PATH"
-# Add a copr repo mirror
-# Note: if a URL starts on a new line, it MUST begin with leading spaces,
-#       otherwise it will be ignored
-sed -i '/baseurl=/a\    http://artifacts.ci.centos.org/systemd/mrc0mmand-systemd-centos-ci/' "$COPR_REPO_PATH"
-sed -i '/gpgkey=/d' "$COPR_REPO_PATH"
-sed -i 's/skip_if_unavailable=True/skip_if_unavailable=False/' "$COPR_REPO_PATH"
-# As the gpgkey directive doesn't support mirrors, let's install the GPG key manually
-if ! rpm --import https://copr-be.cloud.fedoraproject.org/results/mrc0mmand/systemd-centos-ci/pubkey.gpg; then
-    rpm --import http://artifacts.ci.centos.org/systemd/mrc0mmand-systemd-centos-ci/pubkey.gpg
-fi
-yum -q -y install epel-release yum-utils gdb
-yum-config-manager -q --enable epel
-yum -q -y update
-yum -q -y install busybox dnsmasq e2fsprogs gcc-c++ libasan libbpf-devel nc net-tools ninja-build \
-                  pcre2-devel python36 python-lxml qemu-kvm quota strace systemd-ci-environment
-python3.6 -m ensurepip
-python3.6 -m pip install meson
-
-# python36 package doesn't create the python3 symlink
-rm -f /usr/bin/python3
-ln -s "$(which python3.6)" /usr/bin/python3
-
-# Build and install dracut from upstream
-(
-    test -e dracut && rm -rf dracut
-    git clone git://git.kernel.org/pub/scm/boot/dracut/dracut.git
-    pushd dracut || { echo >&2 "Can't pushd to dracut"; exit 1; }
-    git checkout 046
-    ./configure --disable-documentation
-    make -j $(nproc)
-    make install
-    popd
-) 2>&1 | tee "$LOGDIR/dracut-build.log"
+# Install and enable EPEL
+dnf -q -y install epel-release dnf-utils "${ADDITIONAL_DEPS[@]}"
+dnf config-manager -q --enable epel
+# Upgrade the machine to get the most recent environment
+dnf -y upgrade
+# Install systemd's build dependencies
+dnf -q -y --enablerepo "PowerTools" builddep systemd
 
 # Fetch the upstream systemd repo
 test -e systemd && rm -rf systemd
@@ -132,10 +98,6 @@ ninja-build -C build install
     make -C test/TEST-01-BASIC clean setup run clean-again
 ) 2>&1 | tee "$LOGDIR/sanity-boot-check.log"
 
-# Readahead is dead in systemd upstream
-rm -f /usr/lib/systemd/system/systemd-readahead-done.service
-popd
-
 # The systemd testsuite uses the ext4 filesystem for QEMU virtual machines.
 # However, the ext4 module is not included in initramfs by default, because
 # CentOS uses xfs as the default filesystem
@@ -151,7 +113,8 @@ fi
 
 # Set user_namespace.enable=1 (needed for systemd-nspawn -U to work correctly)
 grubby --args="user_namespace.enable=1" --update-kernel="$(grubby --default-kernel)"
-grep "user_namespace.enable=1" /boot/grub2/grub.cfg
+# grub on RHEL 8 uses BLS
+grep -r "user_namespace.enable=1" /boot/loader/entries/
 echo "user.max_user_namespaces=10000" >> /etc/sysctl.conf
 
 echo "-----------------------------"
