f123a2e426ee3af2647ef07d13705d0986975cf0
Frantisek Sumsal <frantisek@sumsal.cz>
2020-01-17 19:30:27 +0100

    jenkins: move the cron build bootstrap into the repo

---
+++ jenkins/runners/systemd-cron-build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Note: this script MUST be self-contained - i.e. it MUST NOT source any
+# external scripts as it is used as a bootstrap script, thus it's
+# fetched and executed without rest of this repository
+#
+# Example usage in Jenkins
+# #!/bin/sh
+#
+# set -e
+#
+# curl -q -o runner.sh https://../systemd-cron-build.sh
+# chmod +x runner.sh
+# ./runner.sh
+
+set -e
+set -o pipefail
+
+at_exit() {
+    # Correctly collect artifacts from all cron jobs and generate a nice
+    # directory structure
+    if find . -name "artifacts_*" | grep -q "."; then
+        mkdir _artifacts_all
+        mv artifacts_* _artifacts_all
+        mv _artifacts_all artifacts_all
+
+        utils/generate-index.sh artifacts_all index.html
+    fi
+}
+
+trap at_exit EXIT
+
+export PATH="/home/systemd/bin:$PATH"
+ARGS=
+
+git clone https://github.com/systemd/systemd-centos-ci
+cd systemd-centos-ci
+
+./agent-control.py --version 8 --no-index --vagrant arch-sanitizers-gcc $ARGS
+#./agent-control.py --version 8 --no-index --vagrant arch-sanitizers-clang $ARGS
