30811bf21389a4cbea0a1fa37c011daf95940ea0
Frantisek Sumsal <fsumsal@redhat.com>
2020-05-26 11:46:18 +0200

    vagrant: dump host's journal before exiting
    Also, make the at_exit() function a bit more robust.

--- vagrant/vagrant-ci-wrapper.sh
+++ vagrant/vagrant-ci-wrapper.sh
@@ -2,25 +2,28 @@
 # Auxiliary script for the CentOS CI infrastructure.
 #
 # This script basically checks out the requested branch of the systemd/systemd
-# repository, install & configures Vagrant, and runs the configured test suite
+# repository, installs & configures Vagrant, and runs the configured test suite
 # in the Vagrant container on given distributions.
 
 # TODO: argument parsing, so we can properly distinguish between branch/commit
 #       and distro tag
 
+LIB_ROOT="$(dirname "$0")/../common"
+. "$LIB_ROOT/utils.sh" || exit 1
+. "$LIB_ROOT/task-control.sh" "vagrant-logs" || exit 1
+
 at_exit() {
     set +e
+    journalctl -b --no-pager > "$LOGDIR/journalctl-vagrant-host.log"
     # Copy over all vagrant-related artifacts, so the Jenkins artifact plugin
     # can gather them for further investigation
-    cp -r $SYSTEMD_ROOT/vagrant-* "$LOGDIR"
+    if [[ -v SYSTEMD_ROOT ]]; then
+        cp -r "$SYSTEMD_ROOT"/vagrant-* "$LOGDIR"
+    fi
     # Collect QEMU serial console logs (see the configuration in a respective
     # Vagrantfile)
     cp /tmp/vagrant-*-console.log "$LOGDIR"
 }
-
-LIB_ROOT="$(dirname "$0")/../common"
-. "$LIB_ROOT/utils.sh" || exit 1
-. "$LIB_ROOT/task-control.sh" "vagrant-logs" || exit 1
 
 REPO_URL="${REPO_URL:-https://github.com/systemd/systemd.git}"
 SCRIPT_ROOT="$(dirname "$0")"
