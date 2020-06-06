b4fb5e9c6c9cf0c6ebff21892a9afe79ca847f29
Frantisek Sumsal <frantisek@sumsal.cz>
2020-06-06 16:21:40 +0200

    hotfix: temporarily disable test-exec-deserialization
    Something broke this test after system upgrade. Let's disable it,
    temporarily, until I get back home to investigate it further.

--- vagrant/vagrant-test.sh
+++ vagrant/vagrant-test.sh
@@ -147,7 +147,7 @@ done
 
 ## Other integration tests ##
 TEST_LIST=(
-    "test/test-exec-deserialization.py"
+#    "test/test-exec-deserialization.py"
     "test/test-network/systemd-networkd-tests.py"
 )
 
