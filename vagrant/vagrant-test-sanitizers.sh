7ee9889b3daaf2c03e9ea7b814e611e82617a1a6
Frantisek Sumsal <frantisek@sumsal.cz>
2020-07-30 14:02:34 +0200

    vagrant: use the built coredumpctl when parsing test journals
    As in the journalctl case, when we attempt to read coredumps from
    journals generated by the tests, we might get into a situation where the
    local journalctl/coredumpctl version lacks necessary features, leading to
    unreadable journals and silently skipped checks.

--- vagrant/vagrant-test-sanitizers.sh
+++ vagrant/vagrant-test-sanitizers.sh
@@ -154,7 +154,7 @@ if [[ $NSPAWN_EC -eq 0 ]]; then
         testdir="/var/tmp/systemd-test-${t##*/}"
         if [[ -f "$testdir/system.journal" ]]; then
             # Attempt to collect coredumps from test-specific journals as well
-            exectask "${t##*/}_coredumpctl_collect" "coredumpctl_collect '$testdir/'"
+            exectask "${t##*/}_coredumpctl_collect" "COREDUMPCTL_BIN='$BUILD_DIR/coredumpctl' coredumpctl_collect '$testdir/'"
             # Check for sanitizer errors in test journals
             exectask "${t##*/}_sanitizer_errors" "$BUILD_DIR/journalctl --file $testdir/system.journal | check_for_sanitizer_errors"
             # Keep the journal files only if the associated test case failed
