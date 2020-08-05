85c490cd0714503225b0eb7ee8d59d5136ba0e8a
Frantisek Sumsal <frantisek@sumsal.cz>
2020-08-05 11:09:06 +0200

    vagrant: bump the "reassessment" wait timeout

--- vagrant/vagrant-build.sh
+++ vagrant/vagrant-build.sh
@@ -135,9 +135,9 @@ fi
 if [[ $SSH_EC -ne 0 ]]; then
     echo >&2 "'vagrant ssh' exited with an unexpected EC: $SSH_EC"
     # Give the VM some time to reassess itself, if the fail is caused by networking
-    sleep 10
+    sleep 60
     # Attempt to dump at least the system journal
-    timeout 2m vagrant ssh -c "sudo journalctl --no-pager -b" > "$SYSTEMD_ROOT/vagrant-journal-dump.log"
+    timeout 60 vagrant ssh -c "sudo journalctl --no-pager -b" > "$SYSTEMD_ROOT/vagrant-journal-dump.log"
     exit $SSH_EC
 fi
 
