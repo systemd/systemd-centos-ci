6de456da307f09b49b0973d56bd438c413ac5df7
Frantisek Sumsal <frantisek@sumsal.cz>
2021-04-29 17:38:35 +0200

    agent: fix the test-journal-flush workaround
    The code was recently modified due to shellcheck changes, so let's tweak
    the workaround to respect that.

--- agent/testsuite.sh
+++ agent/testsuite.sh
@@ -47,7 +47,7 @@ pushd systemd || { echo >&2 "Can't pushd to systemd"; exit 1; }
 #
 # See: systemd/systemd#17963
 # shellcheck disable=SC2016
-sed -i '/TEST_LIST=/aTEST_LIST=("${TEST_LIST[@]/\\/usr\\/lib\\/systemd\\/tests\\/test-journal-flush}")' test/units/testsuite-02.sh
+sed -i '/mapfile -t TEST_LIST/aTEST_LIST=("${TEST_LIST[@]/\\/usr\\/lib\\/systemd\\/tests\\/test-journal-flush}")' test/units/testsuite-02.sh
 
 # FIXME: test-loop-block
 # This test is flaky due to uevent mess, and requires a kernel change.
