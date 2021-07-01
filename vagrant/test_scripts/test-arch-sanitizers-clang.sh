54e82086227612ed396b6b5b99859f7e01b96a9d
Frantisek Sumsal <frantisek@sumsal.cz>
2021-07-01 11:24:43 +0200

    vagrant: temporarily disable test-execute under sanitizers
    This test has been timing out intermittently without any apparent
    reason. Until the root cause is found out, let's skip this test under
    sanitizers to reduce CI flakiness.
    
    e.g.:
    
    ```
    exec-dynamicuser-runtimedirectory3.service: About to execute /bin/sh -x -c 'test -d /run/test-exec_runtimedirectorypreserve'
    exec-dynamicuser-runtimedirectory3.service: Forked /bin/sh as 62440
    exec-dynamicuser-runtimedirectory3.service: Changed dead -> start
    exec-dynamicuser-runtimedirectory2.service: Control group is empty.
    dev-vda2.device: Changed dead -> tentative
    systemd\x2dmeson\x2dbuild-test-tmpfs.mount: Deactivated successfully.
    systemd\x2dmeson\x2dbuild-test-tmpfs.mount: Changed mounted -> dead
    Test timeout when testing exec-dynamicuser-runtimedirectory3.service
    exec-dynamicuser-runtimedirectory3.service: Sent signal SIGKILL to main process 62440 ((sh)) on client request.
    ```
    
    ```
    exec-dynamicuser-runtimedirectory2.service: Passing 0 fds to service
    exec-dynamicuser-runtimedirectory2.service: About to execute /bin/sh -x -c 'test -d /run/test-exec_runtimedirectorypreserve'
    exec-dynamicuser-runtimedirectory2.service: Forked /bin/sh as 73034
    exec-dynamicuser-runtimedirectory2.service: Changed dead -> start
    sys-subsystem-net-devices-test1.device: Changed dead -> plugged
    sys-devices-virtual-net-test1.device: Changed dead -> plugged
    sys-subsystem-net-devices-dummy98.device: Changed dead -> plugged
    sys-devices-virtual-net-dummy98.device: Changed dead -> plugged
    ...
    sys-subsystem-net-devices-vlan99.device: Changed dead -> plugged
    sys-devices-virtual-net-vlan99.device: Changed dead -> plugged
    sys-subsystem-net-devices-n.device: Changed dead -> plugged
    sys-devices-virtual-net-n.device: Changed dead -> plugged
    Test timeout when testing exec-dynamicuser-runtimedirectory2.service
    exec-dynamicuser-runtimedirectory2.service: Sent signal SIGKILL to main process 73034 ((sh)) on client request.
    ```
    
    ```
    exec-dynamicuser-supplementarygroups.service: Passing 0 fds to service
    exec-dynamicuser-supplementarygroups.service: About to execute /bin/sh -x -c 'HAVE=0; for g in $$(id -G); do test "$$g" = "1" && HAVE=1; done; test "$$HAVE" -eq 1'
    exec-dynamicuser-supplementarygroups.service: Forked /bin/sh as 72702
    exec-dynamicuser-supplementarygroups.service: Changed dead -> start
    exec-dynamicuser-fixeduser-one-supplementarygroup.service: Control group is empty.
    sys-subsystem-net-devices-test1.device: Changed dead -> plugged
    sys-devices-virtual-net-test1.device: Changed dead -> plugged
    ...
    sys-subsystem-net-devices-n.device: Changed dead -> plugged
    sys-devices-virtual-net-n.device: Changed dead -> plugged
    Test timeout when testing exec-dynamicuser-supplementarygroups.service
    exec-dynamicuser-supplementarygroups.service: Sent signal SIGKILL to main process 72702 ((sh)) on client request.
    ```

--- vagrant/test_scripts/test-arch-sanitizers-clang.sh
+++ vagrant/test_scripts/test-arch-sanitizers-clang.sh
@@ -56,6 +56,11 @@ echo 'int main(void) { return 77; }' > src/journal/test-journal-flush.c
 #   systemd/systemd#18166
 echo 'int main(void) { return 77; }' > src/test/test-loop-block.c
 
+# FIXME: test-execute
+# This test occasionally timeouts when running under sanitizers. Until the root
+# cause is figured out, let's temporarily skip this test to not disturb CI runs.
+echo 'int main(void) { return 77; }' > src/test/test-execute.c
+
 ## FIXME: systemd-networkd testsuite: skip test_macsec
 # Since kernel 5.7.2 the macsec module is broken, causing a runtime NULL pointer
 # dereference (and since 5.8.0 an additional oops). Since the issue hasn't been
@@ -64,30 +69,8 @@ echo 'int main(void) { return 77; }' > src/test/test-loop-block.c
 # See: systemd/systemd#16199
 sed -i '/def test_macsec/i\    @unittest.skip("See systemd/systemd#16199")' test/test-network/systemd-networkd-tests.py
 
-## Temporary wrapper for `meson test` which disables LSan for `test-execute`
-# LSan keeps randomly crashing during `test-execute` so let's (temporarily)
-# disable it until we find out the culprit.
-# See:
-#   https://github.com/systemd/systemd-centos-ci/pull/217#issuecomment-580717687
-#   https://github.com/systemd/systemd/issues/14598
-ASAN_WRAPPER="$(mktemp "$BUILD_DIR/asan-wrapper-XXX.sh")"
-cat > "$ASAN_WRAPPER" << EOF
-#!/bin/bash
-
-export ASAN_OPTIONS=$ASAN_OPTIONS
-export UBSAN_OPTIONS=$UBSAN_OPTIONS
-
-if [[ \$(basename "\$1") == 'test-execute' ]]; then
-    ASAN_OPTIONS="\$ASAN_OPTIONS:detect_leaks=0"
-fi
-
-exec "\$@"
-EOF
-
-chmod +x "$ASAN_WRAPPER"
-
 # Run the internal unit tests (make check)
-exectask "ninja-test_sanitizers" "meson test -C $BUILD_DIR --wrapper=$ASAN_WRAPPER --print-errorlogs --timeout-multiplier=3"
+exectask "ninja-test_sanitizers" "meson test -C $BUILD_DIR --print-errorlogs --timeout-multiplier=3"
 exectask "check-meson-logs-for-sanitizer-errors" "cat $BUILD_DIR/meson-logs/testlog*.txt | check_for_sanitizer_errors"
 
 ## Run TEST-01-BASIC under sanitizers
