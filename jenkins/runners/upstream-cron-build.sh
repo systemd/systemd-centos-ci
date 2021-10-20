6419ed49d4fe735555c1ba2346ba838f516a0f04
Frantisek Sumsal <frantisek@sumsal.cz>
2021-10-20 12:40:11 +0200

    jenkins: collect test coverage in the cron job

--- jenkins/runners/upstream-cron-build.sh
+++ jenkins/runners/upstream-cron-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2181
 
 # Note: this script MUST be self-contained - i.e. it MUST NOT source any
 # external scripts as it is used as a bootstrap script, thus it's
@@ -30,9 +31,27 @@ at_exit() {
 trap at_exit EXIT
 
 ARGS=()
+EC=0
 
 git clone https://github.com/systemd/systemd-centos-ci
 cd systemd-centos-ci
 
-./agent-control.py --version 8 --no-index --vagrant arch-sanitizers-gcc ${ARGS:+"${ARGS[@]}"}
-#./agent-control.py --version 8 --no-index --vagrant arch-sanitizers-clang ${ARGS:+"${ARGS[@]}"}
+set +e
+
+(
+    set -e
+    # Run the "leftover" ASan/UBSan job (i.e. the one which is not run by
+    # the `upstream-vagrant-archlinux-sanitizers` job for each PR)
+    ./agent-control.py --version 8 --no-index --vagrant arch-sanitizers-gcc ${ARGS:+"${ARGS[@]}"}
+    #./agent-control.py --version 8 --no-index --vagrant arch-sanitizers-clang ${ARGS:+"${ARGS[@]}"}
+)
+[[ $? -ne 0 ]] && EC=$((EC + 1))
+
+(
+    set -e
+    # Collect test coverage & upload it to Coveralls
+    ./agent-control.py --version 8 --no-index --vagrant arch-coverage ${ARGS:+"${ARGS[@]}"}
+)
+[[ $? -ne 0 ]] && EC=$((EC + 1))
+
+exit $EC
