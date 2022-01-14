48f1319acc94e1ce076262d8eab61345a55749fc
Frantisek Sumsal <frantisek@sumsal.cz>
2022-01-14 17:59:29 +0100

    utils: drop EPEL8 from reposync
    since EL8 is going EOL at the end of January (and we already use C8S
    everywhere else in the CI)

--- utils/reposync.sh
+++ utils/reposync.sh
@@ -59,12 +59,6 @@ EOF
     fi
 }
 
-# EPEL-8
-ORIGINAL_REPO="https://copr.fedorainfracloud.org/coprs/mrc0mmand/systemd-centos-ci-centos8/repo/epel-8/mrc0mmand-systemd-centos-ci-centos8-epel-8.repo"
-ORIGINAL_REPO_ID="copr:copr.fedorainfracloud.org:mrc0mmand:systemd-centos-ci-centos8"
-LOCAL_REPO_ID="mrc0mmand-systemd-centos-ci-centos8-epel8"
-sync_repo "$ORIGINAL_REPO" "$ORIGINAL_REPO_ID" "$LOCAL_REPO_ID" "x86_64 aarch64 ppc64le"
-
 # centos-8-stream
 ORIGINAL_REPO="https://copr.fedorainfracloud.org/coprs/mrc0mmand/systemd-centos-ci-centos8/repo/centos-stream-8/mrc0mmand-systemd-centos-ci-centos8-centos-stream-8.repo"
 ORIGINAL_REPO_ID="copr:copr.fedorainfracloud.org:mrc0mmand:systemd-centos-ci-centos8"
