4ba6da25513398ae96549716d66ab9f8b754b350
Frantisek Sumsal <frantisek@sumsal.cz>
2022-03-05 12:00:41 +0100

    jenkins: introduce jobs for the systemd-stable repo

---
+++ jenkins/runners/upstream-vagrant-archlinux-sanitizers-stable.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Note: this script MUST be self-contained - i.e. it MUST NOT source any
+# external scripts as it is used as a bootstrap script, thus it's
+# fetched and executed without rest of this repository
+#
+# Example usage in Jenkins
+# #!/bin/sh
+#
+# set -e
+#
+# curl -q -o runner.sh https://../upstream-vagrant-archlinux-sanitizers-stable.sh
+# chmod +x runner.sh
+# ./runner.sh
+set -eu
+set -o pipefail
+
+at_exit() {
+    # Correctly collect artifacts from all sanitizer jobs and generate a nice
+    # directory structure
+    if find . -name "artifacts_*" | grep -q "."; then
+        mkdir _artifacts_all
+        mv artifacts_* _artifacts_all
+        mv _artifacts_all artifacts_all
+
+        utils/generate-index.sh artifacts_all index.html
+    fi
+}
+
+trap at_exit EXIT
+
+ARGS=()
+
+if [[ -v ghprbPullId && -n "$ghprbPullId" ]]; then
+    ARGS+=(--pr "$ghprbPullId")
+
+    # We're not testing the main branch, so let's see if the PR scope
+    # is something we should indeed test
+    git clone https://github.com/systemd/systemd-stable systemd-tmp && cd systemd-tmp
+    git fetch -fu origin "refs/pull/$ghprbPullId/head:pr"
+    git checkout pr
+    SCOPE_RX='(^(catalog|factory|hwdb|meson.*|network|[^\.].*\.d|rules|src|test|units))'
+    if ! git diff "$(git merge-base main pr)" --name-only | grep -E "$SCOPE_RX" ; then
+        echo "Changes in this PR don't seem relevant, skipping..."
+        exit 0
+    fi
+    cd .. && rm -fr systemd-tmp
+fi
+
+git clone https://github.com/systemd/systemd-centos-ci
+cd systemd-centos-ci
+
+# Run both jobs, since we don't run the other-half-of-this-job in cron in this case
+./agent-control.py --version 8-stream --bootstrap-args='-s' --no-index --vagrant arch-sanitizers-gcc ${ARGS:+"${ARGS[@]}"}
+./agent-control.py --version 8-stream --bootstrap-args='-s' --no-index --vagrant arch-sanitizers-clang ${ARGS:+"${ARGS[@]}"}
---
+++ jenkins/runners/upstream-vagrant-archlinux-stable.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Note: this script MUST be self-contained - i.e. it MUST NOT source any
+# external scripts as it is used as a bootstrap script, thus it's
+# fetched and executed without rest of this repository
+#
+# Example usage in Jenkins
+# #!/bin/sh
+#
+# set -e
+#
+# curl -q -o runner.sh https://../upstream-vagrant-archlinux-stable.sh
+# chmod +x runner.sh
+# ./runner.sh
+set -eu
+set -o pipefail
+
+ARGS=()
+
+if [[ -v ghprbPullId && -n "$ghprbPullId" ]]; then
+    ARGS+=(--pr "$ghprbPullId")
+
+    # We're not testing the main branch, so let's see if the PR scope
+    # is something we should indeed test
+    git clone https://github.com/systemd/systemd-stable systemd-tmp && cd systemd-tmp
+    git fetch -fu origin "refs/pull/$ghprbPullId/head:pr"
+    git checkout pr
+    SCOPE_RX='(^(catalog|factory|hwdb|meson.*|network|[^\.].*\.d|rules|src|test|units))'
+    if ! git diff "$(git merge-base main pr)" --name-only | grep -E "$SCOPE_RX" ; then
+        echo "Changes in this PR don't seem relevant, skipping..."
+        exit 0
+    fi
+    cd .. && rm -fr systemd-tmp
+fi
+
+git clone https://github.com/systemd/systemd-centos-ci
+cd systemd-centos-ci
+
+./agent-control.py --version 8-stream --bootstrap-args='-s' --vagrant arch ${ARGS:+"${ARGS[@]}"}
