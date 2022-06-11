3ed7c6f218bcb3dc37444a1b506d647f4d02ccdd
Frantisek Sumsal <frantisek@sumsal.cz>
2022-06-11 20:28:39 +0200

    jenkins: include the newly introduced C8S ppc64le job
    Also, reduce the code duplication a little (I guess).

--- jenkins/runners/upstream-cron-build.sh
+++ jenkins/runners/upstream-cron-build.sh
@@ -31,27 +31,45 @@ at_exit() {
 trap at_exit EXIT
 
 ARGS=()
+FAILED=()
+PASSED=()
 EC=0
 
 git clone https://github.com/systemd/systemd-centos-ci
 cd systemd-centos-ci
 
-set +e
-
-(
-    set -e
+run_remaining_sanitizer_job() {
     # Run the "leftover" ASan/UBSan job (i.e. the one which is not run by
     # the `upstream-vagrant-archlinux-sanitizers` job for each PR)
     ./agent-control.py --version 8-stream --no-index --vagrant arch-sanitizers-gcc ${ARGS:+"${ARGS[@]}"}
     #./agent-control.py --version 8-stream --no-index --vagrant arch-sanitizers-clang ${ARGS:+"${ARGS[@]}"}
-)
-[[ $? -ne 0 ]] && EC=$((EC + 1))
+}
 
-(
-    set -e
+run_coverage() {
     # Collect test coverage & upload it to Coveralls
     ./agent-control.py --version 8-stream --no-index --vagrant arch-coverage ${ARGS:+"${ARGS[@]}"}
-)
-[[ $? -ne 0 ]] && EC=$((EC + 1))
+}
+
+run_ppc64le_sanitizers() {
+    # Run the integration test suite on C8S ppc64le with sanitizers
+    ./agent-control.py --version 8-stream --arch ppc64le --flavor medium --skip-reboot \
+                       --bootstrap-script="bootstrap-alt.sh" --testsuite-script="testsuite-alt.sh" \
+                       ${ARGS:+"${ARGS[@]}"}
+}
+
+for job in run_remaining_sanitizer_job run_coverage run_ppc64le_sanitizers; do
+    if ! "$job"; then
+        FAILED+=("$job")
+        EC=$((EC + 1))
+    else
+        PASSED+=("$job")
+    fi
+done
+
+echo "PASSED TASKS:"
+printf "    %s\n" "${PASSED[@]}"
+echo
+echo "FAILED TASKS:"
+printf "    %s\n" "${FAILED[@]}"
 
 exit $EC
