f812a60737fb93a53f6155858934343483ba71c5
Frantisek Sumsal <frantisek@sumsal.cz>
2022-07-25 14:18:45 +0200

    jenkins: don't generate the artifact index twice

--- jenkins/runners/vagrant-make-cache.sh
+++ jenkins/runners/vagrant-make-cache.sh
@@ -67,7 +67,7 @@ fi
     echo "Updating the systemd/selinux Fedora Rawhide image"
 
     # Generate a new image with '-new' suffix
-    ./agent-control.py --version 8-stream --vagrant-sync Vagrantfile_rawhide_selinux ${ARGS:+"${ARGS[@]}"}
+    ./agent-control.py --version 8-stream --no-index --vagrant-sync Vagrantfile_rawhide_selinux ${ARGS:+"${ARGS[@]}"}
     # Overwrite the production image with the just tested one. Since the CentOS CI
     # artifact server supports only rsync protocol, use a single-purpose script
     # to do that
