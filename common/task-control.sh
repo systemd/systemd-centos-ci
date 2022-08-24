4703dc5a4c49337f2d9f3e0aed8dde77fb03e6e1
Frantisek Sumsal <frantisek@sumsal.cz>
2022-08-24 18:34:19 +0200

    common: set # of CPUs per VM to 2

--- common/task-control.sh
+++ common/task-control.sh
@@ -24,7 +24,7 @@ declare -ri EXECTASK_RETRY_DEFAULT=3
 # utility. If that fails, fall back to using default values for necessary
 # variables.
 if NPROC=$(nproc); then
-    OPTIMAL_QEMU_SMP=4
+    OPTIMAL_QEMU_SMP=2
     MAX_QUEUE_SIZE=$((NPROC / OPTIMAL_QEMU_SMP))
     if [[ $MAX_QUEUE_SIZE -lt 1 ]]; then
         # We have enough CPUs for only one concurrent task
