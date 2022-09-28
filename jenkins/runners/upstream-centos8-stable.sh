f8f4b1c52fd2491d6afdcc7f4e7d44cbb1928ca6
Frantisek Sumsal <frantisek@sumsal.cz>
2022-09-28 15:18:46 +0200

    jenkins: avoid double-cloning of the systemd repo
    We already have the systemd repo at hand thanks to Jenkins, and
    the GHPRB plugin exports a helpful variable `$ghprbTargetBranch`,
    so we don't have to do the target branch detection ourselves.

--- jenkins/runners/upstream-centos8-stable.sh
+++ jenkins/runners/upstream-centos8-stable.sh
@@ -41,19 +41,14 @@ if [[ -v ghprbPullId && -n "$ghprbPullId" ]]; then
 
     # We're not testing the main branch, so let's see if the PR scope
     # is something we should indeed test
-    git clone https://github.com/systemd/systemd-stable systemd-tmp
-    cd systemd-tmp
-    git fetch -fu origin "refs/pull/$ghprbPullId/head:pr"
-    git checkout pr
+    #
     # Let's make the regex here less strict, so we can, for example, test man page
     # generation and other low-impact changes
     SCOPE_RX='(^(catalog|factory|hwdb|man|meson.*|network|[^\.].*\.d|rules|src|test|tools|units))'
-    MAIN_BRANCH="$(git rev-parse --abbrev-ref origin/HEAD)"
-    if ! git diff "$(git merge-base "$MAIN_BRANCH" pr)" --name-only | grep -E "$SCOPE_RX" ; then
+    if ! git diff "origin/${ghprbTargetBranch:?}" --name-only | grep -E "$SCOPE_RX" ; then
         echo "Changes in this PR don't seem relevant, skipping..."
         exit 0
     fi
-    cd .. && rm -fr systemd-tmp
 fi
 
 git clone https://github.com/systemd/systemd-centos-ci
--- jenkins/runners/upstream-vagrant-archlinux-stable.sh
+++ jenkins/runners/upstream-vagrant-archlinux-stable.sh
@@ -22,17 +22,11 @@ if [[ -v ghprbPullId && -n "$ghprbPullId" ]]; then
 
     # We're not testing the main branch, so let's see if the PR scope
     # is something we should indeed test
-    git clone https://github.com/systemd/systemd-stable systemd-tmp
-    cd systemd-tmp
-    git fetch -fu origin "refs/pull/$ghprbPullId/head:pr"
-    git checkout pr
     SCOPE_RX='(^(catalog|factory|hwdb|meson.*|network|[^\.].*\.d|rules|src|test|units))'
-    MAIN_BRANCH="$(git rev-parse --abbrev-ref origin/HEAD)"
-    if ! git diff "$(git merge-base "$MAIN_BRANCH" pr)" --name-only | grep -E "$SCOPE_RX" ; then
+    if ! git diff "origin/${ghprbTargetBranch:?}" --name-only | grep -E "$SCOPE_RX" ; then
         echo "Changes in this PR don't seem relevant, skipping..."
         exit 0
     fi
-    cd .. && rm -fr systemd-tmp
 fi
 
 git clone https://github.com/systemd/systemd-centos-ci
