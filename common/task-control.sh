systemd/systemd-centos-ci

commit 70e99d70a756ffd0f33e9dea1dd6311f70825e89

common: shorten the sleep before checking a process
This should speed up especially the coredump/sanitizer checks, as they
take < 1s and we run dozens of them in a sequence.

common/task-control.sh
@@ -68,10 +68,10 @@ waitforpid() {
 
     echo "Waiting for PID $pid to finish"
     while kill -0 "$pid" 2>/dev/null; do
-        if ((SECONDS % 10 == 0)); then
+        if ((SECONDS % 100 == 0)); then
             echo -n "."
         fi
-        sleep 1
+        sleep .1
     done
 
     wait "$pid"

commit 01d629d249695ebaef0cdf7342099eb8b70c1a0b

common: fix the progress indicatior in waitforpid()
This was broken since 70e99d7, since that commit bumped the granularity
to milliseconds, but the status indicator still depended on $SECONDS,
causing the indicator to be printed up to ten times every ten seconds.

common/task-control.sh
@@ -60,12 +60,13 @@ echo "[TASK-CONTROL] MAX_QUEUE_SIZE = $MAX_QUEUE_SIZE"
 #   - PID (must be a child of current shell)
 waitforpid() {
     local pid="${1:?Missing PID}"
+    local counter=0
     local ec
     SECONDS=0
 
     _echo "Waiting for PID $pid to finish"
     while kill -0 "$pid" 2>/dev/null; do
-        if ((SECONDS % 100 == 0)); then
+        if ((counter++ % 100 == 0)); then
             _echo -n "."
         fi
         sleep .1