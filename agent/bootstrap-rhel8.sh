2b21d369e262c9594554b1c4e1cdc7ad0f85c83e
Frantisek Sumsal <frantisek@sumsal.cz>
2023-07-03 11:15:11 +0200

    Forward logs to console only in initrd during boot check
    The debug logs are quite noisy and actually slow down the machine boot,
    as the console becomes oversaturated. Since we store the machine journal
    anyway let's forward logs to console only in initrd to have something
    actionable if something blows up during early boot.

--- agent/bootstrap-rhel8.sh
+++ agent/bootstrap-rhel8.sh
@@ -236,7 +236,7 @@ fi
     # (for QEMU tests)
     export KERNEL_BIN="/boot/vmlinuz-$(uname -r)"
     # Enable kernel debug output for easier debugging when something goes south
-    export KERNEL_APPEND="debug systemd.log_level=debug systemd.log_target=console $CGROUP_KERNEL_ARGS"
+    export KERNEL_APPEND="debug systemd.log_level=debug rd.systemd.log_target=console $CGROUP_KERNEL_ARGS"
     # Set timeout for QEMU tests to kill them in case they get stuck
     export QEMU_TIMEOUT=600
     # Disable nspawn version of the test
