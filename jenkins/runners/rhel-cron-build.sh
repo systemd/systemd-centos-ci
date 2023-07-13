c9c794f0d6b8fd68b053131579324c7bc51311c8
Frantisek Sumsal <frantisek@sumsal.cz>
2023-07-13 21:39:56 +0200

    jenkins: split the RHEL9 cron job

--- jenkins/runners/rhel-cron-build.sh
+++ jenkins/runners/rhel-cron-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2181,SC2317,SC2043
+# shellcheck disable=SC2181,SC2317
 
 # Note: this script MUST be self-contained - i.e. it MUST NOT source any
 # external scripts as it is used as a bootstrap script, thus it's
@@ -38,14 +38,16 @@ EC=0
 git clone https://github.com/systemd/systemd-centos-ci
 cd systemd-centos-ci
 
-run_rhel9_full() {
+run_rhel9_full_unified() {
     # C9S job with unified cgroup hierarchy
     ./agent-control.py --no-index --pool virt-ec2-t2-centos-9s-x86_64 \
                        --bootstrap-script="bootstrap-rhel9.sh" \
                        --bootstrap-args="-h unified" \
                        --testsuite-script="testsuite-rhel9.sh" \
                        ${ARGS:+"${ARGS[@]}"}
+}
 
+run_rhel9_full_legacy() {
     # C9S job with legacy cgroup hierarchy
     ./agent-control.py --no-index --pool virt-ec2-t2-centos-9s-x86_64 \
                        --bootstrap-script="bootstrap-rhel9.sh" \
@@ -54,7 +56,7 @@ run_rhel9_full() {
                    ${ARGS:+"${ARGS[@]}"}
 }
 
-for job in run_rhel9_full; do
+for job in run_rhel9_full_{unified,legacy}; do
     if ! "$job"; then
         FAILED+=("$job")
         EC=$((EC + 1))
