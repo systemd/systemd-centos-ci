511bb8352ea826fc8f4bbc56efe1dcb4578eb0e8
Frantisek Sumsal <frantisek@sumsal.cz>
2023-11-10 11:17:17 +0100

    agent(alt): normalize the RT path

--- agent/bootstrap-alt.sh
+++ agent/bootstrap-alt.sh
@@ -176,7 +176,7 @@ fi
 # Unlike gcc's ASan, clang's ASan DSO is in a non-standard path, thus any binary
 # compiled with -shared-libasan using clang will fail to start. Let's add the
 # necessary path to the ldconfig cache to avoid that.
-if ! ASAN_RT_PATH="$(${CC:-clang} --print-file-name "libclang_rt.asan.so")" || ! [[ -f "$ASAN_RT_PATH" ]]; then
+if ! ASAN_RT_PATH="$(readlink -f "$(${CC:-clang} --print-file-name "libclang_rt.asan.so")")" || ! [[ -f "$ASAN_RT_PATH" ]]; then
     echo >&2 "Couldn't detect path to the clang's ASan RT library"
     exit 1
 fi
